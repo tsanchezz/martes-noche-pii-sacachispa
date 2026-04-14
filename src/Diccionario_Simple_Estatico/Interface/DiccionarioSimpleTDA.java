a475868f6270131e9bdf8641ccebeb3e4db1bd92
Tomas Sanchez <158583652+tsanchezz@users.noreply.github.com>
2026-04-14 20:49:30 -0300

    Delete src directory

--- src/Diccionario_Simple_Estatico/Interface/DiccionarioSimpleTDA.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-
-public interface DiccionarioSimpleTDA {
-    void InicializarDiccionario();
-    void Agregar(int clave, int valor);
-    void Eliminar(int clave);
-    int[] Claves();
-}
--- src/Pilas_estaticas/Interfaces/PilaTDA.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-public interface PilaTDA {
-    void InicializarPila();
-    void Apilar(int x);
-    void Desapilar();
-    int Tope();
-    boolean PilaVacia();
-}
